a0f7dae417dc430b3ab279be459b1942afa04829
SeoU <tjdnfkd1225@naver.com>
2024-01-03 18:20:43 +0900

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by 은서우 on 2024/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
